19df956bfcbde919d8c614383e6559f3fb63b84b
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-22 19:34:08 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -30,7 +30,7 @@ usuarios_permitidos = {
     "analopez" : "Analopez.journoAI",
     "valeriaveiga" : "Valeriaveiga.journoAI",
     "alvarorafaelvl" : "Alvarorafaelvl.journoAI",
-    "", ""
+    "": ""
     # Puedes añadir más usuarios aquí
 }
 
