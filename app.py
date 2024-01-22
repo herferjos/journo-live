8492bac1b4863379387fcc09840c277e9a8a89b4
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-22 15:11:03 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+from openai import OpenAI
+import streamlit as st
+import tempfile
+import os
+from modules import *
+from io import BytesIO
+import re
+import html2text
+from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(page_title="Journo.AI", page_icon="🗞️", layout="wide")
+
+st.markdown(
+  """
+  <div style='text-align: center;'>
+      <h1>🗞️ Journo 🗞️</h1>
+      <h4>Tu asistente periodístico de inteligencia artificial</h4>
+  </div>
+  """,
+    unsafe_allow_html=True
+)
+st.write("---")
+
+# # @st.cache_resource(persis="disk")
+# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+# st.session_state.url = "https://docs.google.com/spreadsheets/d/11lvyXKreH17szyJRQcDU9MZWJe0XywpaFhEuBZbxtsw/edit?usp=sharing"
+
+# if 'data' not in st.session_state:
+#   st.session_state.data = conn.read(spreadsheet = st.session_state.url , worksheet = "11lvyXKreH17szyJRQcDU9MZWJe0XywpaFhEuBZbxtsw")
+
+# Inicio de sesión
+if 'autenticado' not in st.session_state:
+    nombre_usuario = st.text_input("Nombre de usuario")
+    contraseña = st.text_input("Contraseña", type="password")
+
+    if st.button("Iniciar sesión", type = "primary"):
+        if verificar_credenciales(nombre_usuario, contraseña):
+            st.session_state['autenticado'] = True
+            st.rerun()
+        else:
+            st.error("Usuario o contraseña incorrectos")
+
+
+if 'autenticado' in st.session_state:
+    if 'temp_path' not in st.session_state:
+        st.success("¡Autenticado con éxito!")
+        st.info("Sube aquí tu archivo de audio con las declaraciones que deseas convertir en una noticia. Asegúrate de que sea un archivo en formato MP3.")
+        archivo = st.file_uploader("Cargar archivo de audio", type=['mp3'])
+        if st.button("Siguiente", type = "primary"):
+          temp_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3").name
+          with open(temp_path, "wb") as f:
+              f.write(archivo.read())
+          st.session_state.temp_path = temp_path
+          st.rerun()
+        
+    if 'temp_path' in st.session_state and 'X' not in st.session_state:
+        st.info("Completa los siguientes campos para proporcionar contexto y detalles específicos que ayudarán a generar la noticia.")
+        X = st.text_input(":blue[¿Cuál es el cargo de la persona que habla?]")
+        Y = st.text_input(":blue[¿Cuál es el nombre de la persona que habla?]")
+        Z = st.text_input(":blue[¿Cuál es el tema más relevante del que ha hablado?]")
+        A = st.text_input(":blue[¿Dónde ha dicho las declaraciones?]")
+        B = st.text_input(":blue[¿Cuándo ha dicho las declaraciones?]")
+        if st.button("Enviar", type = "primary"):
+            st.session_state.X = X
+            st.session_state.Y = Y
+            st.session_state.Z = Z
+            st.session_state.A = A
+            st.session_state.B = B
+
+            with st.spinner("Cargando tu noticia... ⌛"):
+                st.warning("Este proceso puede tardar unos minutos. ¡Recuerda revisarla antes de publicar!")
+                st.session_state.transcription = transcribe_audio(st.session_state.temp_path)
+                st.session_state.noticia_generada = generar_noticia(st.session_state.transcription, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
+                # nueva_fila = {'Transcripcion': st.session_state.transcription, 'X': st.session_state.X, 'Y': st.session_state.Y, 'Z': st.session_state.Z, 'A': st.session_state.A, 'B': st.session_state.B, 'Noticia': st.session_state.noticia_generada}
+                # st.session_state.data = st.session_state.data.append(nueva_fila, ignore_index=True)
+                # conn.update(spreadsheet = st.session_state.url, data=st.session_state.data)
+                st.rerun()
+              
+
+    if 'noticia_generada' in st.session_state:
+        st.write("## ✔️¡Listo! Aquí tienes tu noticia:")
+
+        estilo_bordes_redondeados = """
+            <style>
+                .bordes-redondeados {
+                    border-radius: 10px;
+                    padding: 10px;
+                    border: 2px solid #ccc; /* Puedes ajustar el color del borde según tus preferencias */
+                }
+            </style>
+        """
+
+        # Aplicar el estilo CSS
+        st.markdown(estilo_bordes_redondeados, unsafe_allow_html=True)
+
+        # Mostrar el texto con bordes redondeados
+        st.markdown(f'<div class="bordes-redondeados">{st.session_state.noticia_generada}</div>', unsafe_allow_html=True)
---
+++ modules.py
@@ -0,0 +1,68 @@
+from openai import OpenAI
+import streamlit as st
+import tempfile
+import os
+from io import BytesIO
+import tempfile
+import json
+
+# Configuración de la clave API de OpenAI
+openai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Credenciales de acceso permitidas
+usuarios_permitidos = {
+    "dpelagu": "Dpelagu.journoAI",
+    "jorgepedrosa": "Jorgepedrosa.journoAI",
+    "luciavillalba": "Luciavillalba.journoAI",
+    "mariasanchez" : "Mariasanchez.journoAI",
+    "albarosado" : "Albarosado.journoAI",
+    "juanromera" : "Juanromera.journoAI",
+    "anamontañez" : "Anamontañez.journoAI",
+    "carlosguerrero" : "Carlosguerrero.journoAI",
+    "daninuñez" : "Daninuñez.journoAI",
+    "marmanrique" : "Marmanrique.journoAI",
+    "joseluisherfer" : "Joseluisherfer.journoAI",
+    "borjagutierrez" : "Borjagutierrez.journoAI",
+    "javipachon" : "Javipachon.journoAI",
+    "samuruiz" : "Samuruiz.journoAI",
+    "martapachon" : "Martapachon.journoAI",
+    "josemrodriguez" : "Josemrodriguez.journoAI",
+    "analopez" : "Analopez.journoAI",
+    "valeriaveiga" : "Valeriaveiga.journoAI",
+    "alvarorafaelvl" : "Alvarorafaelvl.journoAI",
+    # Puedes añadir más usuarios aquí
+}
+
+# Verificar las credenciales del usuario
+def verificar_credenciales(nombre_usuario, contraseña):
+    return usuarios_permitidos.get(nombre_usuario) == contraseña
+
+
+# Funciones auxiliares
+def transcribe_audio(file_path):
+    with open(file_path, "rb") as audio_file:
+        transcript_response = openai_client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file
+        )
+        # Accede al texto de la transcripción directamente desde el objeto de respuesta
+        return transcript_response.text
+
+
+def generar_noticia(transcripcion, X, Y, Z, A, B):
+    my_texto = "Vas a actuar como un asistente de inteligencia artificial para periodistas, cuya tarea será redactar un artículo periodístico informativo de la mayor longitud posible utilizando la cantidad máxima de tokens disponibles a partir de declaraciones proporcionadas por un individuo. Te proporcionaré cinco variables clave: X (cargo del individuo), Y (nombre completo del individuo), Z (tema más relevante que protagonizará los primeros párrafos), A (dónde ha dicho las declaraciones) y B (cuándo ha dicho las declaraciones); además de la propias declaraciones. Aquí te detallo el enfoque que debes seguir al redactar el artículo. Considera estas indicaciones paso a paso para asegurarnos de tener la respuesta correcta: 1. Ordena el artículo utilizando la estructura periodística clásica de pirámide invertida, de mayor a menor importancia de los temas tratados. El primer párrafo debe explicar quién ha dicho qué (variable Z), cuándo (B) y dónde (A). Inicia con las declaraciones más directamente relacionadas con Z, que deben situarse en los primeros párrafos. A medida que avances, presenta la información de manera descendente en términos de su relevancia y relación con Z, hasta llegar a las declaraciones menos relevantes y menos relacionadas con el tema principal. 2. Utiliza citas directas entre comillas para presentar las frases y razonamientos del individuo, pero atribúyelas siempre a su autor en el párrafo mediante expresiones como “ha dicho”, “ha indicado” o “ha manifestado”. Mantén una distancia periodística de imparcialidad en todo momento. Tu trabajo es informar de la forma más aséptica posible y citar las declaraciones más valorativas y calificativas entre comillas. No añadas ninguna interpretación o valoración sin entrecomillar a las declaraciones. 3. Utiliza ÚNICAMENTE el pretérito perfecto compuesto durante todo el texto para referirte a las acciones del orador: “ha dicho”, “ha manifestado”, “ha indicado”... Evita en todo momento el uso del pretérito perfecto simple. 4. No añadas información que no esté presente en las declaraciones proporcionadas. El artículo debe basarse únicamente en dichas declaraciones, por lo que debes trabajar dentro de los límites de la información proporcionada. 5. Evita repeticiones tanto de conceptos como de palabras en todo el artículo, asegurándote de mantener una fluidez y legibilidad óptimas. Utiliza sinónimos y expresiones diferentes para mantener la diversidad lingüística"
+    messages = [
+        {"role": "system", "content": f"{my_texto} \n X: {X}, Y: {Y}, Z: {Z}, A: {A}, B: {B}. Declaraciones: {transcripcion}"},
+        {"role": "system", "content": "Debes responder con un JSON de la siguiente forma: {'noticia': <string de la noticia>}"},
+        {"role": "user", "content": "Recuerda que debes devolverme el texto con etiquetas de html para que la noticia sea más visual, utilizando todo lo necesario para destacar el contenido: titulos (h1, h2, h3...), negritas (<b>, <strong>), cursivas (<em>, <i>), subrayados (<u>), etc."},
+    ]
+
+    response_noticia = openai_client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
+        max_tokens=3500,
+        temperature=0,
+        response_format={"type": "json_object"}
+    )
+
+    return json.loads(response_noticia.choices[0].message.content)['noticia']
